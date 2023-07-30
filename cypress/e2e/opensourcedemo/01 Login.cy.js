8aea1678b6ee88a4ad45756b19552016f30c8e4b
Ridho Tiyar <ridhotiyar@ridhotiyar-mbp.local>
2023-07-30 17:59:33 +0800

    logout

--- cypress/e2e/opensourcedemo/01 Login.cy.js
+++ cypress/e2e/opensourcedemo/01 Login.cy.js
@@ -9,11 +9,6 @@ describe('Login', () => {
   })
 
   it('login negative', () => {
-    // cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
-    // cy.get('i.oxd-icon.bi-caret-down-fill.oxd-userdropdown-icon').click()
-    // cy.get('a.oxd-userdropdown-link').click()
-    // cy.get('h5.oxd-text.oxd-text--h5.orangehrm-login-title').contains("Login")
-
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get('input[name="username"]').type('Admin')
     cy.get('input[name="password"]').type('admin')
---
+++ cypress/e2e/opensourcedemo/02 Logout.cy.js
@@ -0,0 +1,12 @@
+describe('logout', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.title('OrangeHRM')
+    cy.get('input[name="username"]').type('Admin')
+    cy.get('input[name="password"]').type('admin123')
+    cy.get('button[class="oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"]').click()
+    cy.get('h6[class="oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module"]').contains('Dashboard')
+
+    cy.get('i.oxd-icon.bi-caret-down-fill.oxd-userdropdown-icon').click()
+    cy.get('a.oxd-userdropdown-link').click()
+    cy.get('h5.oxd-text.oxd-text--h5.orangehrm-login-title').contains("Login")
+})
